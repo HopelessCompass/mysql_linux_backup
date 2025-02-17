0e3c1b14730eb53d3eccc64b69eb15618112ca16
Kristina <31845754+HopelessCompass@users.noreply.github.com>
2025-02-17 16:48:46 +0300

    Update cook_some_backups.sh

--- cook_some_backups.sh
+++ cook_some_backups.sh
@@ -7,6 +7,7 @@ MYSQL_PASSWORD=""
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_PATH="$BACKUP_DIR/$TIMESTAMP"
 DATABASE_NAME=""
+DAYS_TO_KEEP=""
 
 # Creating dir for backup
 mkdir -p "$BACKUP_PATH"
@@ -19,3 +20,6 @@ tar -czf "$BACKUP_PATH.tar.gz" -C "$BACKUP_DIR" "$TIMESTAMP"
 
 # Deleting temp directory with SQL-file
 rm -rf "$BACKUP_PATH"
+
+# Deletin old backups (test)
+find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +$DAYS_TO_KEEP -exec rm {} \;
